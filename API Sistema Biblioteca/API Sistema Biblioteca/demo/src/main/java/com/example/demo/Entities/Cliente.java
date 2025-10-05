6b8cf450bbb23ebcbfb6b4f90c1f391f576dbbe3
Luan Mello <luanmello2005@gmail.com>
2025-10-05 14:00:21 -0300

    Adicionando entidade Cliente e sua interface

---
+++ API Sistema Biblioteca/API Sistema Biblioteca/demo/src/main/java/com/example/demo/Entities/Cliente.java
@@ -0,0 +1,31 @@
+package com.example.demo.Entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Entity
+@Table(name = "cliente")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_cliente;
+
+    @Column(nullable = false)
+    private String nome_cliente;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String telefone;
+
+    @Column
+    private String endereco;
+
+}
