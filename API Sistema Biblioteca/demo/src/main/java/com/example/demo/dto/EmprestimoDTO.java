d6b21a4e704ad5e8562318ea9477aa8725024d1b
ACTZ-_-JONAS2K- <jonasteodorobr@gmail.com>
2025-11-01 17:55:41 -0300

    Feat/Criação EmprestimoController, Service e seus DTOs

--- API Sistema Biblioteca/demo/src/main/java/com/example/demo/dto/EmprestimoDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dto;
-import java.time.LocalDateTime;
-
-public record EmprestimoDTO(
-        Long clienteId,
-        Long livroId,
-        LocalDateTime dataDevolucao) {
-
-}
